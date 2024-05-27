0358d9dfc90985c9ed21b6c0c5128214e6b2a004
ilij4Stanojevic SerbianGuy <ilijastanojevic0208@gmail.com>
2024-05-27 08:36:57 +0000

    Prove in classe

---
+++ PROGETTI CPP/progetto 27.05.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace::std;
+int main(){
+    string name;
+    cout<<"Scrivi il tuo nome: "<<endl;
+    getline(cin,name);
+    cout<<"Benvenuto "<<name<<endl;
+    return 0;
+}
